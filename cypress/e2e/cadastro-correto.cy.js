04ad95c5e96414d0aa2db05a559dce66a386c1f8
Marcelo Moreno <marcelo.moreno@univali.br>
2023-10-12 12:00:41 -0300

    criação massa de dados para teste de cadastro de usuários

--- cypress/e2e/cadastro-correto.cy.js
+++ cypress/e2e/cadastro-correto.cy.js
@@ -1,11 +1,25 @@
 describe('Pagina de cadastro', () => {
-  it('Preencher os campos do formulario corretamente para cadastrar um novo usuario', () => {
-    cy.visit('http://localhost:4200/#/home') 
-    cy.contains('a', 'Register now').click();   
-    cy.get('[data-test="email"]').type('maisnada@gmail.com');
-    cy.get('[data-test="fullName"]').type('Marcelo Palmito');
-    cy.get('[data-test="registerUserName"]').type('maisnada');
-    cy.get('[data-test="registerPassword"]').type('maisnada123');
-    cy.contains('button', 'Register').click();
+
+  beforeEach(() => {
+
+    cy.visit('http://localhost:4200/#/home')
+
+  }) 
+
+  const usuarios = require('../fixtures/usuarios.json')
+
+  usuarios.forEach(usuario => {
+
+    it('Preencher os campos do formulario corretamente para cadastrar um novo usuario', () => {
+     
+      cy.get('[data-test="register"]').click();  
+      cy.get('input[data-test="email"]').type(usuario.email);
+      cy.get('input[data-test="fullName"]').type(usuario.fullName);
+      cy.get('input[data-test="registerUserName"]').type(usuario.userName);
+      cy.get('input[data-test="registerPassword"]').type(usuario.password);      
+      cy.get('[data-test="btnRegister"]').click();
+    })
+
   })
+ 
 })

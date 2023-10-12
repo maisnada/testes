26e408ca576c562ad4a3bbe9abaf053617672af9
Marcelo Moreno <marcelo.moreno@univali.br>
2023-10-12 12:12:46 -0300

    teste imagem via API

--- cypress/e2e/api-alurapic.cy.js
+++ cypress/e2e/api-alurapic.cy.js
@@ -1,5 +1,5 @@
 describe('API Alurapic', () => {
-    it('Dados da API', () => {
+   /*  it('Dados da API', () => {
       
         cy.request({
             method: 'POST',
@@ -10,6 +10,20 @@ describe('API Alurapic', () => {
             expect(res.body).is.not.empty
             expect(res.body).to.have.property('id')
             expect(res.body.id).to.be.equal(1)
+        })
+    }) */
+
+    it('Fotos do usuário', () => {
+      
+        cy.request({
+            method: 'GET',
+            url: 'http://localhost:3000/flavio/photos'
+        }).then((res) => {
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            //res.body[0] pega a primeira imagem do body
+            expect(res.body[0]).to.have.property('description')
+            expect(res.body[0].description).to.be.equal('lablink')
         })
     })
   })

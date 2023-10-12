1640391162d2129916090e6c58255aa35f2aaac1
Marcelo Moreno <marcelo.moreno@univali.br>
2023-10-11 22:05:50 -0300

    adição mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,15 @@
-module.exports = {
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+      setupNodeEvents(on, config) {
+          { "reporter"; "mochawesome",
+              "reporterOptions"; 
+                  { "reportDir"; "cypress/report/mochawesome-report",
+                  "overwrite"; true,
+                  "html"; true,
+                  "json"; false,
+                  "timestamp"; "mmddyyyy_HHMMss" }}
+      },
   },
-};
+});

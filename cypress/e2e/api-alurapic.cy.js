ebddefb48d541e5b71b008c2655458fe4998d572
Marcelo Moreno <marcelo.moreno@univali.br>
2023-10-12 11:38:48 -0300

    teste login via API com dados .env

---
+++ cypress/e2e/api-alurapic.cy.js
@@ -0,0 +1,15 @@
+describe('API Alurapic', () => {
+    it('Dados da API', () => {
+      
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3000/user/login',
+            body: Cypress.env()
+        }).then((res) => {
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body).to.have.property('id')
+            expect(res.body.id).to.be.equal(1)
+        })
+    })
+  })

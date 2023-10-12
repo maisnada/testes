cf9e21e79686c676403fdd5cd02a5601ea62cb14
Marcelo Moreno <marcelo.moreno@univali.br>
2023-10-12 10:19:39 -0300

    mochawesome reports html e json

--- cypress/e2e/login-correto.cy.js
+++ cypress/e2e/login-correto.cy.js
@@ -1,11 +1,21 @@
 describe('Pagina de login', () => {
+
+  //todos os passos precisaram da execução desse comando
+  beforeEach(() => {
+
+    cy.visit('http://localhost:4200/#/home')
+
+  }) 
+
     it('Preenche os campos lo login corretamente para realizar login', () => {
-      cy.visit('http://localhost:4200/#/home') 
+      /*cy.visit('http://localhost:4200/#/home') 
       cy.get('[data-test="loginUserName"]').type('flavio');
       cy.get('[data-test="loginPassword"]').type('123');
-      cy.contains('button', 'login').click();  
+      cy.contains('button', 'login').click(); */ 
+
+      //comando customizado em support/commands
+      cy.login('flavio','123');
     })
   })
 
 
-  

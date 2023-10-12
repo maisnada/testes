5c2c6c40b742adbc1fd6a48c84b0521a39ba1e8e
Marcelo Moreno <marcelo.moreno@univali.br>
2023-10-12 11:10:14 -0300

    criação stub para retorno API login

--- cypress/e2e/cadastro-incorreto.cy.js
+++ cypress/e2e/cadastro-incorreto.cy.js
@@ -1,6 +1,12 @@
 describe('Pagina de cadastro', () => {
+
+    beforeEach(() => {
+
+      cy.visit('http://localhost:4200/#/home')
+    }) 
+
     it('Preencher os campos do formulario de forma incorreta', () => {
-      cy.visit('http://localhost:4200/#/home') 
+      //cy.visit('http://localhost:4200/#/home') 
       cy.contains('a', 'Register now').click();
       cy.contains('button', 'Register').click();
       cy.contains('button', 'Register').click();
--- cypress/e2e/login-incorreto.cy.js
+++ cypress/e2e/login-incorreto.cy.js
@@ -1,9 +1,28 @@
 describe('Pagina de login', () => {
+
+    beforeEach(() => {
+
+      cy.visit('http://localhost:4200/#/home')
+
+      cy.intercept('POST', 'http://localhost:3000/user/login', {
+        statusCode: 401
+      }).as('stubPost')//dublê de post que irá implementar o erro 400
+
+      //https://www.alura.com.br/artigos/testes-com-mocks-e-stubs
+      //https://docs.cypress.io/api/commands/intercept#__docusaurus_skipToContent_fallback
+    }) 
+
     it('Verifica mensagem de campos obrigatorios', () => {
-      cy.visit('http://localhost:4200/#/home') 
+      //cy.visit('http://localhost:4200/#/home') 
       cy.contains('User name is required!').should('be.visible');      
       cy.contains('Password is required!').should('be.visible');    
     })
+
+    it('Deve falhar mesmo que os campos sejam preenchidos corretamente', () => {
+      cy.login('flavio','123');
+      cy.wait('@stubPost');   
+    })
+
   })
 
 

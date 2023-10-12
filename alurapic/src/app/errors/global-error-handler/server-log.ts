1f4f428afc52e2776875f2e8904028a8d5855184
Marcelo Moreno <marcelo.moreno@univali.br>
2023-10-11 21:03:32 -0300

    delete alurapic

--- alurapic/src/app/errors/global-error-handler/server-log.ts
+++
@@ -1,6 +0,0 @@
-export interface ServerLog {
-    message: string;
-    url: string;
-    userName: string;
-    stack: string;
-}
--- alurapic/src/app/errors/global-error/global-error.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    templateUrl: './global-error.component.html'
-})
-export class GlobalErrorComponent {}
